4b331897b6b85ca73e5b26fea6163e0a7e9131e5
Edgardo Barría Melián <edgardo.barriam@gmail.com>
2020-09-04 10:33:23 -0400

    init commit

---
+++ src/main/java/io/github/edgardobarriam/springgcpchallenge/SpringGcpChallengeApplication.java
@@ -0,0 +1,13 @@
+package io.github.edgardobarriam.springgcpchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGcpChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGcpChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/edgardobarriam/springgcpchallenge/SpringGcpChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.edgardobarriam.springgcpchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGcpChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

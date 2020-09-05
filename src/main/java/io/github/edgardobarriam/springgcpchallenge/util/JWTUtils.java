cdd5de6c5d4924c92a45100ff892addc8b75722e
Edgardo Barría Melián <edgardo.barriam@gmail.com>
2020-09-05 11:21:03 -0400

    Change JWTUtils to interface

--- src/main/java/io/github/edgardobarriam/springgcpchallenge/util/JWTUtils.java
+++ src/main/java/io/github/edgardobarriam/springgcpchallenge/util/JWTUtils.java
@@ -5,24 +5,26 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.stereotype.Component;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public final class JWTUtils {
+@Component
+public interface JWTUtils {
   
-  private static final String jwtSecret = "mySuperSecureSecret";
-  public static final String AUTHENTICATION_HEADER = "Authorization";
-  public static final String AUTHENTICATION_PREFIX = "Bearer ";
+  String jwtSecret = "mySuperSecureSecret";
+  String AUTHENTICATION_HEADER = "Authorization";
+  String AUTHENTICATION_PREFIX = "Bearer ";
   
-  public static String getJWTToken() {
+  static String getJWTToken() {
     
     /* This method should perform some sort of user/password validation, but considering this is an example, credentials
      are not being validated. */
-  
+    
     List<GrantedAuthority> grantedAuthorities = AuthorityUtils
       .commaSeparatedStringToAuthorityList("ROLE_USER");
-  
+    
     String token = Jwts
       .builder()
       .setId("gcpJWT")
@@ -34,11 +36,11 @@ public final class JWTUtils {
       .setExpiration(new Date(System.currentTimeMillis() + 600000))
       .signWith(SignatureAlgorithm.HS512,
         jwtSecret.getBytes()).compact();
-  
+    
     return AUTHENTICATION_PREFIX + token;
   }
   
-  public static Claims validateToken(String jwtToken) {
+  static Claims validateToken(String jwtToken) {
     return Jwts.parser().setSigningKey(JWTUtils.jwtSecret.getBytes()).parseClaimsJws(jwtToken).getBody();
   }
 }
